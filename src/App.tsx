e40cb4ee2b4445cdae6d0e75c4d2dc8c86022d47
KirillKozlo <kirillkozlogit@gmail.com>
2023-07-09 21:12:40 +0300

    No-MEMO

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Todolists} from "./components/Todolists/Todolists";
+import {useSelector} from "react-redux";
+import {RootReduceType} from "./components/reduce/Store";
 
 function App() {
+    console.log(useSelector<RootReduceType, RootReduceType>(state => state))
   return (
     <div className="App">
-   
+   <Todolists/>
     </div>
   );
 }
